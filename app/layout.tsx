c3972586f8db56364e7f2ba58556682a75574c15
Your Name <awawgas@gmail.com>
2026-07-25 16:51:46 +0500

    Your commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export const metadata: Metadata = {
   title: {
     default: "BitumenCalcPro — Free Bitumen & Asphalt Calculator",
@@ -62,6 +63,8 @@ export default function RootLayout({
         <Navbar/>
       <main className="flex-grow">
           {children}
+                <SpeedInsights />
+
       </main>
       <Footer />
       </body>
